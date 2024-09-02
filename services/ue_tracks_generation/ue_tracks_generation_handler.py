7e3c0b10b021bc75ddc0114a74a0f33cdc3493da
tanzim10 <tanzimfarhan@gmail.com>
2024-09-02 11:57:39 +0600

    UEGeneration Handler Done

--- services/ue_tracks_generation/ue_tracks_generation_handler.py
+++ services/ue_tracks_generation/ue_tracks_generation_handler.py
@@ -12,6 +12,7 @@ from radp.common import constants
 from radp.digital_twin.mobility.ue_tracks import UETracksGenerator
 from radp.digital_twin.utils.gis_tools import GISTools
 from radp.digital_twin.mobility.ue_tracks import MobilityClass
+from ue_tracks_generation.ue_tracks_generation_helper import UETracksGenerationHelper
 
 
 
@@ -259,8 +260,354 @@ class UETracksGenerationHandler:
 
 
 class UETracksGenerationParamHandler:
+  '''
+    The UETracksGenerationParamHandler Class handles execution of the UE Tracks Generation parameterss
 
-    def __init__(self, params: Dict):
+    The UE Tracks Generation Service will take in as input an UE Tracks Generation job
+    with the following format:
+    
+    "ue_tracks_generation": {
+            "output_file_prefix": "",
+            "params": {
+                "simulation_duration": 3600,
+                "simulation_time_interval": 0.01,
+                "num_ticks": 100,
+                "num_batches": 10,
+                "ue_class_distribution": {
+                    "stationary": {
+                        "count": 0,
+                        "velocity": 1,
+                        "velocity_variance": 1
+                    },
+                    "pedestrian": {
+                        "count": 0,
+                        "velocity": 1,
+                        "velocity_variance": 1
+                    },
+                    "cyclist": {
+                        "count": 0,
+                        "velocity": 1,
+                        "velocity_variance": 1
+                    },
+                    "car": {
+                        "count": 0,
+                        "velocity": 1,
+                        "velocity_variance": 1
+                    }
+                },
+                "lat_lon_boundaries": {
+                    "min_lat": -90,
+                    "max_lat": 90,
+                    "min_lon": -180,
+                    "max_lon": 180
+                },
+                "gauss_markov_params": {
+                    "alpha": 0.5,
+                    "variance": 0.8,
+                    "rng_seed": 42,
+                    "lon_x_dims": 100,
+                    "lon_y_dims": 100
+                    "// TODO": "Account for supporting the user choosing the anchor_loc and cov_around_anchor.",
+                    "// Current implementation": "the UE Tracks generator will not be using these values.",
+                    "// anchor_loc": {},
+                    "// cov_around_anchor": {}
+                }
+            }
+        }
+    '''
+
+
+
+  def __init__(self, params: Dict):
         self.params = params
+        self.rng_seed = None
+        self.num_batches = None
+        self.lon_x_dims = None
+        self.lon_y_dims = None
+        self.num_ticks = None
+        self.num_UEs = None
+        self.alpha = None
+        self.variance = None
+        self.min_lat = None
+        self.max_lat = None
+        self.min_lon = None
+        self.max_lon = None
+        self.mobility_class_distribution = {}
+        self.mobility_class_velocities = {}
+        self.mobility_class_velocity_variances = {}
+        self.simulation_interval = None
+        self.simulation_time = None
+
+
+  def extract_ue_data(self):
+      """
+        Extracts necessary data from the parameters to prepare for UE track generation.
+      """
+
+      ue_tracks = self.params[constants.UE_TRACKS_GENERATION][constants.PARAMS]
+      self.num_batches = ue_tracks[constants.NUM_BATCHES]
+      self.lon_x_dims = ue_tracks[constants.GAUSS_MARKOV_PARAMS][constants.LON_X_DIMS]
+      self.lon_y_dims = ue_tracks[constants.GAUSS_MARKOV_PARAMS][constants.LON_Y_DIMS]
+      self.num_ticks = ue_tracks[constants.NUM_TICKS]
+      self.alpha = ue_tracks[constants.GAUSS_MARKOV_PARAMS][constants.ALPHA]
+      self.variance = ue_tracks[constants.GAUSS_MARKOV_PARAMS][constants.VARIANCE]
+      self.min_lat = ue_tracks[constants.LON_LAT_BOUNDARIES][constants.MIN_LAT]
+      self.max_lat = ue_tracks[constants.LON_LAT_BOUNDARIES][constants.MAX_LAT]
+      self.min_lon = ue_tracks[constants.LON_LAT_BOUNDARIES][constants.MIN_LON]
+      self.max_lon = ue_tracks[constants.LON_LAT_BOUNDARIES][constants.MAX_LON]
+      self.rng_seed = ue_tracks[constants.GAUSS_MARKOV_PARAMS][constants.RNG_SEED]
+      self.simulation_interval = ue_tracks[constants.SIMULATION_TIME_INTERVAL]
+
+
+  def extract_ue_class_distribution(self):
+
+      """
+        Processes and calculates UE class distribution, velocities, and variances from the parameters.
+      """
+
+      ue_tracks = self.params[constants.UE_TRACKS_GENERATION][constants.PARAMS]
+      simulation_time_interval = self.params[constants.UE_TRACKS_GENERATION][constants.PARAMS][constants.SIMULATION_TIME_INTERVAL]
+
+      #Get the total number of UEs from the UE class distribution and add them up
+      (
+          stationary_count,
+          pedestrian_count,
+          cyclist_count,
+          car_count,
+      ) = UETracksGenerationHelper.get_ue_class_distribution_count(ue_tracks)
+
+      self.num_UEs = stationary_count + pedestrian_count + cyclist_count + car_count
+      
+      # Calculate the mobility class distribution as provided
+      stationary_distribution = stationary_count / self.num_UEs
+      pedestrian_distribution = pedestrian_count / self.num_UEs
+      cyclist_distribution = cyclist_count / self.num_UEs
+      car_distribution = car_count / self.num_UEs
+
+      # Create the mobility class distribution dictionary
+      self.mobility_class_distribution = {
+              MobilityClass.stationary: stationary_distribution,
+              MobilityClass.pedestrian: pedestrian_distribution,
+              MobilityClass.cyclist: cyclist_distribution,
+              MobilityClass.car: car_distribution,
+          }
+
+      # Calculate the velocity class for each UE class
+      # Each velocity class will be calculated according to the simulation_time_interval provided by the user,
+      # which indicates the unit of time in seconds.
+      # Each grid here defined in the mobility model is assumed to be 1 meter
+      # Hence the velocity will have a unit of m/s (meter/second)
+      (
+              stationary_velocity,
+              pedestrian_velocity,
+              cyclist_velocity,
+              car_velocity,
+      ) = UETracksGenerationHelper.get_ue_class_distribution_velocity(ue_tracks,simulation_time_interval)
+                                                                      
+                                                                      
+
+      self.mobility_class_velocities = {
+        MobilityClass.stationary: stationary_velocity,
+        MobilityClass.pedestrian: pedestrian_velocity,
+        MobilityClass.cyclist: cyclist_velocity,
+        MobilityClass.car: car_velocity,
+      }
+
+      # Calculate the velocity variance for each UE class
+      # Each velocity variance will be calculated according to the simulation_time_interval provided by the user,
+      # which indicates the unit of time in seconds.
+      # Each grid here defined in the mobility model is assumed to be 1 meter
+      # Hence the velocity will have a unit of m/s (meter/second)
+      (
+              stationary_velocity_variance,
+              pedestrian_velocity_variance,
+              cyclist_velocity_variance,
+              car_velocity_variances,
+      ) = UETracksGenerationHelper.get_ue_class_distribution_velocity_variances(
+              ue_tracks,simulation_time_interval
+      )
+
+      self.mobility_class_velocity_variances = {
+              MobilityClass.stationary: stationary_velocity_variance,
+              MobilityClass.pedestrian: pedestrian_velocity_variance,
+              MobilityClass.cyclist: cyclist_velocity_variance,
+              MobilityClass.car: car_velocity_variances,
+      }
+
+      print(self.mobility_class_distribution)
+      print(self.mobility_class_velocities)
+      print(self.mobility_class_velocity_variances)
+      print(self.num_UEs)
+
+  def generate_ue_data(self) -> List[List[Tuple[float, float]]]:
+
+    """
+      Generates UE data based on the extracted parameters and returns a list of batches of UE coordinates.
+
+      Returns:
+          List[List[Tuple[float, float]]]: A list of batches, each containing lists of UE coordinates.
+    """
+          
+    self.extract_ue_data()
+    self.extract_ue_class_distribution()
+    ue_generator = UETracksGenerator(
+        rng=np.random.default_rng(self.rng_seed),
+        mobility_class_distribution=self.mobility_class_distribution,
+        mobility_class_velocities=self.mobility_class_velocities,
+        mobility_class_velocity_variances=self.mobility_class_velocity_variances,
+        lon_x_dims=self.lon_x_dims,
+        lon_y_dims=self.lon_y_dims,
+        num_ticks=self.num_ticks,
+        num_UEs=self.num_UEs,
+        alpha = self.alpha,
+        variance = self.variance,
+        min_lat = self.min_lat,
+        max_lat = self.max_lat,
+        min_lon = self.min_lon,
+        max_lon = self.max_lon
+        )
+
+
+    all_batches = []
+    for batch_no in range(self.num_batches):
+        batch = next(ue_generator.generate())
+        all_batches.append(batch)
+    print("Batches of Data: " , all_batches)
+    return all_batches
+
+  def mobility_data_generation(self) -> pd.DataFrame:
+
+      """
+        Processes the generated UE data into a structured DataFrame representing mobility data.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing processed mobility data.
+      """
+      ue_data = self.generate_ue_data()
+      all_dataframes = []  # List to hold all batch dataframes
+
+      # Iterate through each batch
+      for batch_no, xy_batches in enumerate(ue_data):
+          batch_dataframe_list = []
+
+          # Iterate through ticks in each batch
+          for tick, xy_batch in enumerate(xy_batches):
+              # Transforming (x, y) into (lon, lat)
+              lon_lat_pairs = GISTools.converting_xy_points_into_lonlat_pairs(
+                  xy_batch,
+                  self.lon_x_dims,
+                  self.lon_y_dims,
+                  self.min_lon,
+                  self.max_lon,
+                  self.min_lat,
+                  self.max_lat
+              )
+
+              # Building DataFrame for this tick
+              df_tick = pd.DataFrame({
+                  'mock_ue_id': range(self.num_UEs),
+                  'longitude': [pair[0] for pair in lon_lat_pairs],
+                  'latitude': [pair[1] for pair in lon_lat_pairs],
+                  'tick': np.full(self.num_UEs, tick)
+              })
+
+              batch_dataframe_list.append(df_tick)
+
+          # Concatenate all ticks data for the current batch
+          all_dataframes.append(pd.concat(batch_dataframe_list, ignore_index=True))
+
+          if batch_no + 1 >= self.num_batches:  # Check if the specified number of batches has been reached
+              break
+
+      # Concatenate all batches into a single DataFrame and return
+      return pd.concat(all_dataframes, ignore_index=True)
+
+
+  def plot_ue_tracks(self,csv_file: str):
+
+
+        """
+        Plots UE tracks from the data specified in a CSV file.
+
+        Args:
+            csv_file (str): Path to the CSV file containing UE track data.
+        """
+        
+        # Load the data
+        data = pd.read_csv(csv_file)
+
+        # Initialize an empty list to store batch indices
+        batch_indices = []
+
+        # Identify where tick resets and mark the indices
+        for i in range(1, len(data)):
+            if data.loc[i, 'tick'] == 0 and data.loc[i-1, 'tick'] != 0:
+                batch_indices.append(i)
+
+        # Add the final index to close the last batch
+        batch_indices.append(len(data))
+
+        # Now, iterate over the identified batches
+        start_idx = 0
+        for batch_num, end_idx in enumerate(batch_indices):
+            batch_data = data.iloc[start_idx:end_idx]
+
+            # Create a new figure
+            plt.figure(figsize=(10, 6))
+
+            # Generate a color map with different colors for each ue_id
+            color_map = cm.get_cmap('tab20', len(batch_data['mock_ue_id'].unique()))
+
+            # Plot each ue_id's movement over ticks in this batch
+            for idx, ue_id in enumerate(batch_data['mock_ue_id'].unique()):
+                ue_data = batch_data[batch_data['mock_ue_id'] == ue_id]
+                color = color_map(idx)  # Get a unique color for each ue_id
+
+                # Plot the path with arrows
+                for i in range(len(ue_data) - 1):
+                    x_start = ue_data.iloc[i]['longitude']
+                    y_start = ue_data.iloc[i]['latitude']
+                    x_end = ue_data.iloc[i+1]['longitude']
+                    y_end = ue_data.iloc[i+1]['latitude']
+
+                    # Calculate the direction vector
+                    dx = x_end - x_start
+                    dy = y_end - y_start
+
+                    # Plot the line with an arrow with reduced width and unique color
+                    plt.quiver(x_start, y_start, dx, dy, angles='xy', scale_units='xy', scale=1, color=color,
+                              width=0.002, headwidth=3, headlength=5)
+
+                # Plot starting points as circles with the same color
+                plt.scatter(ue_data['longitude'].iloc[0], ue_data['latitude'].iloc[0], color=color, label=f'Start UE {ue_id}')
+
+            # Set plot title and labels
+            plt.title(f'UE Tracks with Direction for Batch {batch_num + 1}')
+            plt.xlabel('Longitude')
+            plt.ylabel('Latitude')
+            plt.legend()
+
+            # Display the plot
+            plt.show()
+
+            # Update start_idx for the next batch
+            start_idx = end_idx
+
+  def save_data_to_csv(self, filename: str):
+        """
+        Save the UE tracks data to a CSV file.
+
+        :param filename: The name of the file to save the CSV data to.
+        """
+        # Generate the data
+        data = self.mobility_data_generation()
+
+        # Save DataFrame to CSV
+        data.to_csv(filename, index=False)
+        print(f"Data successfully saved to {filename}")
+
+
+
+
 
 
