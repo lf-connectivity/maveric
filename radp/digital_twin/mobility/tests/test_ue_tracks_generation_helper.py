4239ca542a589d8ecb1733d004a1f6b60733b1ca
Menon Pranto <menonpranto@gmail.com>
2024-09-20 11:40:04 +0600

    fixed name and removed redundant code in test

--- radp/digital_twin/mobility/tests/test_ue_tracks_generation_helper.py
+++ radp/digital_twin/mobility/tests/test_ue_tracks_generation_helper.py
@@ -97,9 +97,3 @@ class TestUETracksGenerationHelper(unittest.TestCase):
             self.job_data[constants.UE_TRACKS_GENERATION][constants.PARAMS]
         )
         self.assertEqual(alpha, 0.8)
-
-
-"""
-if __name__ == '__main__':
-    unittest.main()
-"""
