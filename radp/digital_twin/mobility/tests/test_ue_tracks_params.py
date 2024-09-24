bca1045e46595d5a9c4fa1cb6c50ae62d3a2d079
Menon Pranto <menonpranto@gmail.com>
2024-09-24 13:08:19 +0600

    fixed test_ue_params

--- radp/digital_twin/mobility/tests/test_ue_tracks_params.py
+++ radp/digital_twin/mobility/tests/test_ue_tracks_params.py
@@ -4,7 +4,8 @@ from services.ue_tracks_generation.ue_tracks_generation_helper import (
     UETracksGenerationHelper,
 )
 from radp.common import constants
-from unittest.mock import patch, MagicMock
+from radp.digital_twin.mobility.ue_tracks import MobilityClass
+from unittest.mock import patch
 
 
 class TestUETracksParams(unittest.TestCase):
@@ -20,46 +21,49 @@ class TestUETracksParams(unittest.TestCase):
                         constants.STATIONARY: {
                             constants.COUNT: 10,
                             constants.VELOCITY: 1,
-                            constants.VELOCITY_VARIANCE: 1
+                            constants.VELOCITY_VARIANCE: 1,
                         },
                         constants.PEDESTRIAN: {
                             constants.COUNT: 10,
                             constants.VELOCITY: 1,
-                            constants.VELOCITY_VARIANCE: 1
+                            constants.VELOCITY_VARIANCE: 1,
                         },
                         constants.CYCLIST: {
                             constants.COUNT: 10,
                             constants.VELOCITY: 1,
-                            constants.VELOCITY_VARIANCE: 1
+                            constants.VELOCITY_VARIANCE: 1,
                         },
                         constants.CAR: {
                             constants.COUNT: 10,
                             constants.VELOCITY: 1,
-                            constants.VELOCITY_VARIANCE: 1
-                        }
+                            constants.VELOCITY_VARIANCE: 1,
+                        },
                     },
                     constants.LON_LAT_BOUNDARIES: {
                         constants.MIN_LAT: -90,
                         constants.MAX_LAT: 90,
                         constants.MIN_LON: -180,
-                        constants.MAX_LON: 180
+                        constants.MAX_LON: 180,
                     },
                     constants.GAUSS_MARKOV_PARAMS: {
                         constants.ALPHA: 0.5,
                         constants.VARIANCE: 0.8,
                         constants.RNG_SEED: 42,
                         constants.LON_X_DIMS: 100,
-                        constants.LON_Y_DIMS: 100
-                    }
+                        constants.LON_Y_DIMS: 100,
+                    },
                 }
             }
         }
 
-    @patch.object(UETracksGenerationHelper, 'get_ue_class_distribution_count')
-    @patch.object(UETracksGenerationHelper, 'get_ue_class_distribution_velocity')
-    @patch.object(UETracksGenerationHelper, 'get_ue_class_distribution_velocity_variances')
-
-    def test_initialization_and_extraction(self, mock_velocity_variances, mock_velocity, mock_count):
+    @patch.object(UETracksGenerationHelper, "get_ue_class_distribution_count")
+    @patch.object(UETracksGenerationHelper, "get_ue_class_distribution_velocity")
+    @patch.object(
+        UETracksGenerationHelper, "get_ue_class_distribution_velocity_variances"
+    )
+    def test_initialization_and_extraction(
+        self, mock_velocity_variances, mock_velocity, mock_count
+    ):
         # Set up the mock return values
         mock_count.return_value = (10, 10, 10, 10)
         mock_velocity.return_value = (1, 1, 1, 1)
@@ -82,20 +86,34 @@ class TestUETracksParams(unittest.TestCase):
         self.assertEqual(params.min_lon, -180)
         self.assertEqual(params.max_lon, 180)
 
-        # Assert mobility class distributions
-        self.assertAlmostEqual(params.mobility_class_distribution[constants.STATIONARY], 0.25)
-        self.assertAlmostEqual(params.mobility_class_distribution[constants.PEDESTRIAN], 0.25)
-        self.assertAlmostEqual(params.mobility_class_distribution[constants.CYCLIST], 0.25)
-        self.assertAlmostEqual(params.mobility_class_distribution[constants.CAR], 0.25)
+        # Assert mobility class distributions using the MobilityClass Enum
+        self.assertAlmostEqual(
+            params.mobility_class_distribution[MobilityClass.stationary], 10 / 40
+        )
+        self.assertAlmostEqual(
+            params.mobility_class_distribution[MobilityClass.pedestrian], 10 / 40
+        )
+        self.assertAlmostEqual(
+            params.mobility_class_distribution[MobilityClass.cyclist], 10 / 40
+        )
+        self.assertAlmostEqual(
+            params.mobility_class_distribution[MobilityClass.car], 10 / 40
+        )
 
         # Assert mobility class velocities
-        self.assertEqual(params.mobility_class_velocities[constants.STATIONARY], 1)
-        self.assertEqual(params.mobility_class_velocities[constants.PEDESTRIAN], 1)
-        self.assertEqual(params.mobility_class_velocities[constants.CYCLIST], 1)
-        self.assertEqual(params.mobility_class_velocities[constants.CAR], 1)
+        self.assertEqual(params.mobility_class_velocities[MobilityClass.stationary], 1)
+        self.assertEqual(params.mobility_class_velocities[MobilityClass.pedestrian], 1)
+        self.assertEqual(params.mobility_class_velocities[MobilityClass.cyclist], 1)
+        self.assertEqual(params.mobility_class_velocities[MobilityClass.car], 1)
 
         # Assert mobility class velocity variances
-        self.assertEqual(params.mobility_class_velocity_variances[constants.STATIONARY], 1)
-        self.assertEqual(params.mobility_class_velocity_variances[constants.PEDESTRIAN], 1)
-        self.assertEqual(params.mobility_class_velocity_variances[constants.CYCLIST], 1)
-        self.assertEqual(params.mobility_class_velocity_variances[constants.CAR], 1)
+        self.assertEqual(
+            params.mobility_class_velocity_variances[MobilityClass.stationary], 1
+        )
+        self.assertEqual(
+            params.mobility_class_velocity_variances[MobilityClass.pedestrian], 1
+        )
+        self.assertEqual(
+            params.mobility_class_velocity_variances[MobilityClass.cyclist], 1
+        )
+        self.assertEqual(params.mobility_class_velocity_variances[MobilityClass.car], 1)
